c8bff2e5fe84413e76f9628f79b7a4054218cfab
Lucas Marinho Gomes <lucasmarinhogomes@MacBook-Pro-de-Lucas.local>
2023-01-16 13:36:27 -0300

    start project

---
+++ src/main/java/br/com/lm/votapi/VotapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.lm.votapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotapiApplication.class, args);
+	}
+
+}
